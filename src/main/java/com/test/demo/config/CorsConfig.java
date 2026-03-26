8873d1bdad989bbbb817e48b4c600e494938e5ec
Abhishek Dhankhar <AbhishekDhankhar@bdo.in>
2026-03-26 14:48:41 +0530

    cors

--- src/main/java/com/test/demo/config/CorsConfig.java
+++ src/main/java/com/test/demo/config/CorsConfig.java
@@ -9,11 +9,11 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4201")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+    	registry.addMapping("/**")
+        .allowedOriginPatterns("*")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true)
+        .maxAge(3600);
     }
 }
